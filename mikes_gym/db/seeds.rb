2428033627658bc5ce0e3eb1c487856d2ead0a44
Chris Jennings <chrisjennings555@gmail.com>
2018-11-09 11:36:19 +0000

    gymclasses added to db

--- mikes_gym/db/seeds.rb
+++ mikes_gym/db/seeds.rb
@@ -2,15 +2,22 @@ require_relative("../models/member")
 require_relative("../models/gymclass")
 
 Member.delete_all
+GymClass.delete_all
 
 member1 = Member.new({'first_name' => 'Chris', 'last_name' => 'Jennings', 'wallet' => 10, 'member_type' => 'gold'})
 
 member2 = Member.new({'first_name' => 'Mike', 'last_name' => 'Monteith', 'wallet' => 20, 'member_type' => 'gold'})
 
-
 member1.save
 member2.save
-# member1.delete
 
-member1.member_type = "silver"
-member1.update
+gymclass1 = GymClass.new({'class_name' => 'Spin', 'class_tier' => 'gold', 'class_capacity' => 10})
+
+gymclass2 = GymClass.new({'class_name' => 'Circuits', 'class_tier' => 'silver', 'class_capacity' => 12})
+
+gymclass1.save
+gymclass2.save
+# # member1.delete
+#
+# member1.member_type = "silver"
+# member1.update
--- mikes_gym/models/gymclass.rb
+++ mikes_gym/models/gymclass.rb
@@ -2,4 +2,27 @@ require_relative("../db/sql_runner.rb")
 
 class GymClass
 
+  attr_reader :class_capacity
+  attr_accessor :class_name, :class_tier
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @class_name = options['class_name']
+    @class_tier = options['class_tier']
+    @class_capacity = options['class_capacity']
+  end
+
+  def save()
+    sql = "INSERT INTO classes (class_name, class_tier, class_capacity) VALUES ($1, $2, $3) RETURNING id"
+    values = [@class_name, @class_tier, @class_capacity]
+    result = SqlRunner.run(sql, values)
+    id = result.first['id']
+    @id = id.to_i
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM classes"
+    SqlRunner.run(sql)
+  end
+  
 end
