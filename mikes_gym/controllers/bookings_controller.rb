e48b5b463d1a5bb37e94ca0de16f8b45eaa35dad
Chris Jennings <chrisjennings555@gmail.com>
2018-11-14 11:03:57 +0000

    fully functional

--- mikes_gym/controllers/bookings_controller.rb
+++ mikes_gym/controllers/bookings_controller.rb
@@ -20,12 +20,31 @@ end
 post '/bookings' do
   booking = Booking.new(params)
   booking.save
-  redirect to "/bookings"
+  redirect to ("/bookings")
 end
 
 get '/bookings/:id' do
   @booking = Booking.find(params['id'])
   @member_at_booking = @booking.member
-  @class_at_booking = @booking.class 
+  @class_at_booking = @booking.class
   erb(:"/bookings/show")
+end
+
+get '/bookings/:id/edit' do
+  @members = Member.all
+  @gymclasses = GymClass.all
+  @booking = Booking.find(params['id'])
+  erb(:'/bookings/edit')
+end
+
+post '/bookings/:id' do
+  booking = Booking.new(params)
+  booking.update
+  redirect to "/bookings/#{params['id']}"
+end
+
+post '/bookings/:id/delete' do
+  booking = Booking.find(params['id'])
+  booking.delete
+  redirect to "/bookings"
 end
